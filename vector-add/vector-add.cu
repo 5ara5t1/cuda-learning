afcadd8fefbcab3de3e07f1e43294b1c07d3ef22
5ara5t1 <sarasti-5@pm.me>
2026-05-16 19:13:39 +0000

    added nsys_easy script, implemented prefetch and threading / block sizing.

--- vector-add/vector-add.cu
+++ vector-add/vector-add.cu
@@ -14,15 +14,17 @@
 
 // Kernel function to add the elements for two arrays
 __global__
-void add(int n, float *sum, float *x, float *y) {
-    for(int i = 0; i < n; i++) {
-        sum[i] = x[i] + y[i];
+void add(int n, float *x, float *y) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for(int i = index; i < n; i += stride) {
+        y[i] = x[i] + y[i];
     }
 }
 
 int main(void) {
     int N = 1<<20; // 1M elements
-    float *x, *y, *sum; // variable pointers
+    float *x, *y; // variable pointers
 
     // Allocate Unified Memory - accessible from CPU or GPU
     cudaMallocManaged(&x, N*sizeof(float));
@@ -34,11 +36,20 @@ int main(void) {
         y[i] = 2.0f;
     }
 
+    // Prefetch the x and y arrays to the GPU
+    cudaMemPrefetchAsync(x, N*sizeof(float), 0, 0);
+    cudaMemPrefetchAsync(y, N*sizeof(float), 0, 0);
+   
+
+    // Run kernel on 1M elements on the GPU, specify threadblocks and blocksize
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    
     // capture start time (CPU FOR)
     auto start = std::chrono::steady_clock::now();
 
-    // Run kernel on 1M elements on the GPU
-    add<<<1, 1>>>(N, sum, x, y);
+    // launch kernel
+    add<<<numBlocks, blockSize>>>(N, x, y);
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
