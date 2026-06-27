f0c24ffa4c05c0cd7a4f53712d4e91511103098b
5ara5t1 <sarasti-5@pm.me>
2026-06-27 04:00:44 +0000

    implementing cuda kernal.

--- pmpp-4e/ch03_multidimensional_grids_and_data/color_to_grayscale_conversion/main.cu
+++ pmpp-4e/ch03_multidimensional_grids_and_data/color_to_grayscale_conversion/main.cu
@@ -7,7 +7,41 @@ char *imageName = "testImage.jpg";
 char *imageOut  = "imageOut.jpg";
 int w, h, c;
 
+// colorToGrayscaleConversion - taken from pmpp 4e, figure 3.4
+__global__
+void colortoGrayscaleConversion(unsigned char * Pout, unsigned char * Pin, int width, int height) {
+    int col = blockIdx.x*blockDimx + threadIdx.x;
+    int row = blockIdx.y*blockDim.y + thradIdx.y;
 
+    if (col < width && row < height) {
+        // Get ID offset for the grayscale image
+        int grayOffset = row*width + col;
+        // One can think of the RGB image having CHANNEL
+        // times more columns than the gray scale image
+        int rgbOffset = grayOffset*CHANNELS;
+        unsigned char r = Pin[rgbOffset     ];
+        unsigned char g = Pin[rgbOffset + 1 ];
+        unsigned char b = Pin[rgbOffset + 2 ];
+        // Perform the rescaling and store it
+        // We multiply by floating point constants
+        Pout[grayOffset] = 0.21f*r + 0.71f*g + 0.07f*b;    
+    }
+}
+
+void c2g(unsigned char * Img_out, unsigned char * Img_h, int width, int height) {
+    // Allocate memory on CUDA Device
+    int size = width * height * sizeof(char);
+    cudaMalloc((void **) &Img_d, size);
+    
+    // Copy char array to CUDA Device
+    cudaMemcopy(Img_d, Img_h, size, cudaMemcpyHostToDevice);
+
+    // Invoke Kernal, check for errors
+    dim3 dimGrid(2,)
+
+
+    // Copy char array to host Device
+}
 
 
 // main entry function
